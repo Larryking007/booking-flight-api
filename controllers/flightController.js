0092961ff475b8ddd679ed3143bd0d988d6423ee
Larryking007 <ajikobilanre007@gmail.com>
2022-06-22 23:32:14 +0100

    flight data

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,18 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
+const Flights = require("../models/Flight");
 
+// get all flights
+exports.getFlights = async (req, res) => {
+  try {
+    const Flight = Flights;
+    res.status(200).json(flights);
+  } catch {
+    res.status(500).json({ message: err });
+  }
+};
+// get single flight
 
+//create new flight
+
+// update/edit flight
+
+// delete flight
